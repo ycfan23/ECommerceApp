7eae6fea9fcb6dc93c174781f13c204a1c84ebc7
Yachao Fan <61946174+ycfan23@users.noreply.github.com>
2022-01-05 15:41:29 -0600

    images added for product

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -5,8 +5,8 @@ import Product from './Product/Product'
 
 //dummy data
 const products = [
-  { id: 1, name: 'Shoes', description: 'Running shoes', price: '$5'},
-  { id: 2, name: 'Macbook', description: 'Apple computer', price: '$10'}
+  { id: 1, name: 'Shoes', description: 'Running shoes', price: '$5', image: 'https://static01.nyt.com/images/2020/02/25/well/PHYSED-SHOES1/merlin_168154896_a69879b8-d43a-40d0-8297-dd7086d7d784-articleLarge.jpg?quality=75&auto=webp&disable=upscale'},
+  { id: 2, name: 'Macbook', description: 'Apple computer', price: '$10', image: 'https://media.cnn.com/api/v1/images/stellar/prod/211025072623-macbook-pro-14-display-5.jpg?q=x_0,y_0,h_2268,w_4030,c_fill/h_720,w_1280'}
 ]
 
 
