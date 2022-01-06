b0a7fa5d56a1947781f1301897e04dd1f3a72e11
Yachao Fan <61946174+ycfan23@users.noreply.github.com>
2022-01-06 16:26:44 -0600

    Cart display updated

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, Typography, Button, Grid } from '@material-ui/core';
 import { Link } from 'react-router-dom';
+import CartItem from './CartItem/CartItem'
 
 import useStyles from './styles';
 
@@ -16,7 +17,7 @@ const Cart = ({ cart }) => {
       <Grid container spacing={3}>
         {cart.line_items.map((item) => (
           <Grid item xs={12} sm={4} key={item.id}>
-            <div>{item.name}</div>
+            <CartItem item={item} />
           </Grid>
         ))}
       </Grid>
@@ -35,7 +36,7 @@ const Cart = ({ cart }) => {
   return (
     <Container>
       <div className={classes.toolbar} />
-      <Typography className={classes.title} variant='h3'>Your Shopping Cart</Typography>
+      <Typography className={classes.title} variant='h3' gutterBottom>Your Shopping Cart</Typography>
       { !cart.line_items.length ? <EmptyCart /> : <FilledCart />}
     </Container>
   )
