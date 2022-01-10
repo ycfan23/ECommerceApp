022930d0e60b2040ccd0a0563b8c426195c20a5a
Yachao Fan <61946174+ycfan23@users.noreply.github.com>
2022-01-10 10:56:13 -0600

    AddressForm added

--- src/Components/CheckoutForm/AddressForm.jsx
+++ src/Components/CheckoutForm/AddressForm.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core';
+import { useForm, FormProvider } from 'react-hook-form';
+
+import FormInput from './InputForm'
 
 const AddressForm = () => {
+  const methods = useForm();
+
   return (
-    <div>
-      Address Form
-    </div>
+    <>
+      <Typography variant='h6' gutterBottom>Shipping Address</Typography>
+      <FormProvider {...methods}>
+        <form onSubmit=''>
+          <Grid container spacing={3}>
+            <FormInput required name='firstName' label='First name'/>
+          </Grid>
+        </form>
+      </FormProvider>
+    </>
   )
 }
 
